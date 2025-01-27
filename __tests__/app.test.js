2a044472b5ba84136c10194d8c70bc7abd4c10df
nxvafps <oliverquirk11@gmail.com>
2025-01-27 10:35:15 +0000

    add error handling middleware to catch all errors and also logging unexpected errors

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,15 @@ afterAll(() => {
   db.end();
 });
 
+describe("invalid route", () => {
+  it("should respond with a status code of 404 when given an invalid route", async () => {
+    const {
+      body: { message },
+    } = await request(app).get("/api/invalid").expect(404);
+    expect(message).toBe(`Can't find /api/invalid on this server`);
+  });
+});
+
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", async () => {
     const {
--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const apiRouter = require("./routes/api.routes");
+const errorHandler = require("./middlewares/error-handler");
+const AppError = require("./utils/app-error");
 
 const app = express();
 
 app.use("/api", apiRouter);
+
+app.all("*", (req, res, next) => {
+  next(AppError.notFound(`Can't find ${req.originalUrl} on this server`));
+});
+
+app.use(errorHandler);
 
 module.exports = app;
