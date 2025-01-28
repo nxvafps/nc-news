f3f9908add0dfd6d5d73da93e1ea8e9b5c563755
nxvafps <oliverquirk11@gmail.com>
2025-01-28 01:13:19 +0000

    fix hosting issues

--- app/listen.js
+++ app/listen.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const db = require("../db/connection");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
