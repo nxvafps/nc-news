fbe8afab8440c6f81199de3077d6450d7e17c932
nxvafps <oliverquirk11@gmail.com>
2025-01-29 15:26:36 +0000

    add PATCH /api/users/:username method to enable users to update their user info

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,4 +1,11 @@
-const { selectUsers, selectUserByUsername } = require("../models/user.model");
+const { findUserByUsername } = require("../models/auth.model");
+const {
+  selectUsers,
+  selectUserByUsername,
+  updateUserProfileById,
+} = require("../models/user.model");
+
+const AppError = require("../utils/app-error");
 
 exports.getUsers = async (req, res, next) => {
   try {
@@ -14,6 +21,26 @@ exports.getUserByUsername = async (req, res, next) => {
 
   try {
     const user = await selectUserByUsername(username);
+    res.status(200).send({ user });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateUserProfile = async (req, res, next) => {
+  try {
+    const { username } = req.params;
+    const { name, avatar_url } = req.body;
+
+    const checkUser = await findUserByUsername(username);
+
+    if (!checkUser) throw AppError.notFound("User not found");
+
+    if (username !== req.user.username) {
+      throw AppError.forbidden("Cannot update other users");
+    }
+
+    const user = await updateUserProfileById(username, { name, avatar_url });
     res.status(200).send({ user });
   } catch (err) {
     next(err);
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -24,3 +24,22 @@ exports.selectUserByUsername = async (username) => {
 
   return result.rows[0];
 };
+
+exports.updateUserProfileById = async (username, updates) => {
+  const { name, avatar_url } = updates;
+
+  const result = await db.query(
+    `UPDATE users
+     SET name = COALESCE($1, name),
+         avatar_url = COALESCE($2, avatar_url)
+     WHERE username = $3
+     RETURNING username, name, avatar_url`,
+    [name, avatar_url, username]
+  );
+
+  if (result.rows.length === 0) {
+    throw AppError.notFound("User not found");
+  }
+
+  return result.rows[0];
+};
